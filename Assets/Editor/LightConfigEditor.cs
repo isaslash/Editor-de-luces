a80102aadc4c04b0a6c36b1bf19d06dee61d19c3
facucastejonv96 <31113484+facucastejonv96@users.noreply.github.com>
2017-10-17 19:58:28 -0300

    Add files via upload

---
+++ Assets/Editor/LightConfigEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LightsConfig))]
+public class LightsEditor : Editor
+{
+    private LightsConfig _target;
+
+    private void OnEnable()
+    {
+        _target = (LightsConfig)target;
+    }
+    public override void OnInspectorGUI()
+    {
+
+        base.OnInspectorGUI();
+        _target.name = EditorGUILayout.TextField("Name", _target.name);
+
+
+    }
+}
+
---
+++ Assets/Editor/LigthsConfigCreator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class LigthsConfigCreator {
+
+    [MenuItem("utilities/create Light Config")]
+    public static void CreateLightConfig(){
+        ScriptableObjectUtility.CreateAsset<LightsConfig>();
+    }
+}
