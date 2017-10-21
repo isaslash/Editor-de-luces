4f3ef37e0834bc3363a1a2bf2065409927e48319
isaslash <isaiaslizaso@gmail.com>
2017-10-21 13:10:58 -0300

    Add files via upload
    actualizaciones ya mostradas en clase

--- Assets/Editor/LightConfigEditor.cs
+++ Assets/Editor/LightConfigEditor.cs
@@ -16,9 +16,15 @@ public class LightsEditor : Editor
     {
 
         base.OnInspectorGUI();
+		EditorGUILayout.LabelField ("configuracion de luces", EditorStyles.boldLabel);
         _target.name = EditorGUILayout.TextField("Name", _target.name);
+		_target.bounceIntencity = EditorGUILayout.FloatField ("Bounce Intencity",_target.bounceIntencity);
+		_target.spotValor = EditorGUILayout.FloatField ("Spot Angle", _target.spotValor);
+		_target.intensity = EditorGUILayout.FloatField ("Intencidad", _target.intensity);
+		_target.color = EditorGUILayout.ColorField ("color", _target.color);
 
-
+		//_target.spotValor = EditorGUILayout.Slider ("spotAngle", _target.spotValor, 0, 100);
+		//EditorGUI.ProgressBar (GUILayoutUtility.GetRect (15, 15, "textField"), _target.spotValor / 100, _target.spotValor + "%");
     }
 }
 
--- Assets/Script/LightsConfig.cs
+++ Assets/Script/LightsConfig.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LightsConfig : ScriptableObject {
+	
     public LightType type;
     public Color color;
+	public float spotValor;
     public float intensity;
+	public Light light;
+	public float bounceIntencity;
+
+
 }
