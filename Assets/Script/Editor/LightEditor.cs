00960667161fa12f41d8298df9d665b66e020b00
facucastejonv96 <31113484+facucastejonv96@users.noreply.github.com>
2017-11-03 10:28:04 -0300

    Add files via upload

---
+++ Assets/Script/Editor/LightEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Light))]
+public class LightEditor : Editor {
+
+    public LightsConfig data;
+    private Light _target;
+
+    private void OnEnable()
+    {
+        _target = (Light)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        data = (LightsConfig)EditorGUILayout.ObjectField("Config", data, typeof(LightsConfig), true);
+        if (data != null) {
+            _target.intensity = data.intensity;
+            _target.color = data.color;
+            _target.spotAngle = data.spotValor;
+            _target.bounceIntensity = data.bounceIntencity;
+            _target.type = data.type;
+        }
+    }
+    public void acutalizar()
+    {
+        _target.intensity = data.intensity;
+        _target.color = data.color;
+        _target.spotAngle = data.spotValor;
+        _target.bounceIntensity = data.bounceIntencity;
+        _target.type = data.type;
+    }
+}
---
+++ Assets/Script/Editor/LightsCreator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Threading;
+
+public class LightsCreator : EditorWindow
+{
+    public LightsCreator config;
+    
+
+    [MenuItem("utilities/create Light")]
+    static void CreateWindow() // Crea la ventana a mostrar
+    {
+        ((LightsCreator)GetWindow(typeof(LightsCreator))).Show();
+    }
+
+}
