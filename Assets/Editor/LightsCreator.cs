864bd55117386b0497bb09138272d16cd3a4a3f6
facucastejonv96 <31113484+facucastejonv96@users.noreply.github.com>
2017-11-09 12:25:02 -0300

    Add files via upload

--- Assets/Editor/LightsCreator.cs
+++ Assets/Editor/LightsCreator.cs
@@ -6,13 +6,41 @@ using System.Threading;
 
 public class LightsCreator : EditorWindow
 {
-    public LightsCreator config;
+    string name;
+    float bounceintensity;
+    float spot;
+    float intensity;
+    Color color;
     
 
     [MenuItem("utilities/create Light")]
     static void CreateWindow() // Crea la ventana a mostrar
     {
         ((LightsCreator)GetWindow(typeof(LightsCreator))).Show();
+    }
+    void OnGUI()
+    {
+        name = EditorGUILayout.TextField("Name", name);
+        bounceintensity = EditorGUILayout.FloatField("Bounce Intencity", bounceintensity);
+        spot = EditorGUILayout.FloatField("Spot Angle", spot);
+        intensity = EditorGUILayout.FloatField("Intensidad", intensity);
+        color = EditorGUILayout.ColorField("color", color);
+        Rect rectOpenNew = EditorGUILayout.BeginHorizontal("Button");
+        if (GUI.Button(rectOpenNew, GUIContent.none))
+        {
+            LightsConfig lc = ScriptableObjectUtility.CreateAssetAndReturn<LightsConfig>();
+            lc.name = name;
+            lc.bounceIntencity = bounceintensity;
+            lc.spotValor = spot;
+            lc.intensity = intensity;
+            lc.color = color;
+
+
+        }
+
+        GUILayout.Label("create light");
+        EditorGUILayout.EndHorizontal();
+
     }
 
 }
--- Assets/Editor/ScriptableObjectUtility.cs
+++ Assets/Editor/ScriptableObjectUtility.cs
@@ -27,4 +27,29 @@ public static class ScriptableObjectUtility
         //Marcamos como seleccionado el asset que acabamos de crear
         Selection.activeObject = asset;
     }
+    public static T CreateAssetAndReturn<T>() where T : ScriptableObject
+    {
+        //Creamos la instancia del asset
+        T asset = ScriptableObject.CreateInstance<T>();
+
+        //Creamos la ubicación donde vamos a guardar el asset
+        string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath("Assets/" + typeof(T).ToString() + ".asset");
+
+        //Creamos el asset
+        AssetDatabase.CreateAsset(asset, assetPathAndName);
+
+        //Guardamos los assets en disco
+        AssetDatabase.SaveAssets();
+
+        //Importa los assets que fueron modificados
+        AssetDatabase.Refresh();
+
+        //Ponemos el foco en la ventana de proyecto
+        EditorUtility.FocusProjectWindow();
+
+        //Marcamos como seleccionado el asset que acabamos de crear
+        Selection.activeObject = asset;
+
+        return asset;
+    }
 }
